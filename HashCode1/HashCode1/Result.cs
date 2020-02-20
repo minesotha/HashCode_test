06d66ad9c739ae62d93f4ffb325af335120fa95f
Sotha <minesotha@gmail.com>
2020-02-20 21:51:17 +0100

    kolejka dla jednej bilbioteki

---
+++ HashCode1/HashCode1/Result.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode1
+{
+    public class Result
+    {
+        public int libraryId;
+        public int[] scannedBooksIds;
+
+        public Result(int libraryId, int[] scannedBooksIds)
+        {
+            this.libraryId = libraryId;
+            this.scannedBooksIds = scannedBooksIds;
+        }
+
+        public string getResultLine()
+        {
+            string line = $"{libraryId} {scannedBooksIds.Length}\n";
+            for (int i = 0; i < scannedBooksIds.Length; i++)
+            {
+                line += $"{scannedBooksIds[i]}";
+            }
+
+            return line;
+        }
+    }
+}
