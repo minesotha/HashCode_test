4619f2ba6bb3d86f9e9f429cec63cdeba6bfa7af
Piotr Ciwiś <piotr@ciwis.pl>
2020-02-20 19:47:38 +0100

    Bieżące staty biblioteki.

---
+++ HashCode1/HashCode1/LibraryStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode1
+{
+    class LibraryStats
+    {
+        public Library Library { get; set; }
+        public int Score { get; set; }
+    }
+}
